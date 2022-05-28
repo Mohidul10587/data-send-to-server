ec6d53b8e7c3ad3d779c72820f309518271186c2
Mohid <mohid10587@gmail.com>
2022-05-28 09:34:49 +0600

    practice with data send

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,13 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+
+
+
+
+
+
+        
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
